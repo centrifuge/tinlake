dc73af307ad262b42b935db60d2d2d986cebd957
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-06-28 17:37:28 +0200

    test: added simple unit tests for pile (#15)
    * added pile unit test for borrow and withdraw
    
    * added repay unit test

--- src/test/pile.t.sol
+++ src/test/pile.t.sol
@@ -22,9 +22,9 @@ import "./mock/token.sol";
 
 
 contract PileTest is DSTest {
-
     Pile pile;
     TokenMock tkn;
+
     function setUp() public {
 
         tkn = new TokenMock();
@@ -34,5 +34,74 @@ contract PileTest is DSTest {
     function testSetupPrecondition() public {
         tkn.setBalanceOfReturn(0);
         assertEq(pile.want(),0);
+    }
+
+    function borrow(uint loan, uint wad) public {
+        uint totalBalance = pile.Balance();
+        uint totalDebt = pile.Debt();
+
+        pile.borrow(loan, wad);
+
+        (uint debt, uint balance, uint fee, uint  chi) = pile.loans(loan);
+        assertEq(pile.Balance(), totalBalance + wad);
+        assertEq(pile.Debt(), totalBalance + wad);
+        assertEq(debt, wad);
+        assertEq(balance, wad);
+        assertEq(fee, 0);
+        assertEq(fee, 0);
+    }
+
+    function withdraw(uint loan, uint wad) public {
+        uint totalBalance = pile.Balance();
+        (,uint balance, ,) = pile.loans(loan);
+        assertEq(balance,wad);
+
+        pile.withdraw(loan,wad,address(this));
+
+        assertEq(totalBalance-wad, pile.Balance());
+        (,uint newBalance, ,) = pile.loans(loan);
+        assertEq(balance-wad, newBalance);
+        assertEq(tkn.transferFromCalls(),1);
+
+        assertEq(tkn.dst(),address(pile));
+        assertEq(tkn.src(),address(this));
+        assertEq(tkn.wad(),wad);
+    }
+
+    function repay(uint loan, uint wad) public {
+        uint totalDebt = pile.Debt();
+
+        pile.repay(loan, wad, address(this));
+
+        (uint debt,uint balance, ,) = pile.loans(loan);
+        assertEq(totalDebt-wad, pile.Debt());
+        assertEq(debt,0);
+        assertEq(balance,0);
+
+        assertEq(tkn.transferFromCalls(),2);
+        assertEq(tkn.dst(),address(this));
+        assertEq(tkn.src(),address(pile));
+        assertEq(tkn.wad(),wad);
+
+    }
+
+    function testSimpleBorrow() public {
+        uint loan  = 1;
+        uint wad = 100;
+        borrow(loan,wad);
+    }
+
+    function testSimpleWithdraw() public {
+        uint loan  = 1;
+        uint wad = 100;
+        borrow(loan,wad);
+        withdraw(loan, wad);
+    }
+    function testSimpleRepay() public {
+        uint loan  = 1;
+        uint wad = 100;
+        borrow(loan,wad);
+        withdraw(loan, wad);
+        repay(loan, wad);
     }
 }
