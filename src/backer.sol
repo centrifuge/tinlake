c85dc2a5b60b2cdc5af382150c17952ee16a3970
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-07-03 21:11:52 +0200

    feat: added backer as a new lender adapter (#16)
    * added pile unit test for borrow and withdraw
    
    * added repay unit test
    
    * added backer.sol
    
    * dapp install ds-token
    
    * added tests for backer.sol
    
    * removed backer addr from constructor

---
+++ src/backer.sol
@@ -0,0 +1,56 @@
+pragma solidity >=0.4.24;
+
+contract TokenLike {
+    function transferFrom(address, address, uint) public;
+    function mint(address, uint) public;
+    function approve(address usr, uint wad) public returns (bool);
+    function balanceOf(address usr) public returns (uint);
+}
+
+contract BackerFab {
+    function deploy(address tkn_, address collateral_ ,address lightswitch_) public returns (address) {
+        Backer lender = new Backer(tkn_, collateral_);
+        lender.rely(msg.sender);
+        lender.deny(address(this));
+        return address(lender);
+    }
+}
+
+contract Backer {
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address usr) public auth { wards[usr] = 1; }
+    function deny(address usr) public auth { wards[usr] = 0; }
+    modifier auth { require(wards[msg.sender] == 1); _; }
+
+    // --- Data ---
+    TokenLike public tkn;
+    TokenLike public collateral;
+
+    address private backer;
+
+    constructor (address tkn_, address collateral_) public {
+        wards[msg.sender] = 1;
+        tkn = TokenLike(tkn_);
+        collateral = TokenLike(collateral_);
+    }
+
+    // --- Backer Methods ---
+    function provide(address usrC, address usrT, uint wadC, uint wadT) auth public {
+        require(tkn.balanceOf(backer)>= wadT);
+        collateral.transferFrom(usrC, backer, wadC);
+        tkn.transferFrom(backer, usrT, wadT);
+
+    }
+
+    function release(address usrC, address usrT, uint wadC, uint wadT) auth  public {
+        require(collateral.balanceOf(backer)>= wadC);
+        tkn.transferFrom(usrT, backer, wadT);
+        collateral.transferFrom(backer, usrC, wadC);
+    }
+
+    function file(address usr) auth public {
+        backer = usr;
+    }
+
+}
