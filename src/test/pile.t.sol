797684e5547b5e5abf3ddaf1a5368a2e460d1a21
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-07-04 20:23:28 +0200

    tests: additional pile tests (#18)

--- src/test/pile.t.sol
+++ src/test/pile.t.sol
@@ -118,6 +118,48 @@ contract PileTest is DSTest {
         return rad_ / 10 ** 27;
     }
 
+    // --- Math ---
+    uint256 constant ONE = 10 ** 27;
+    function rpow(uint x, uint n, uint base) internal pure returns (uint z) {
+        assembly {
+            switch x case 0 {switch n case 0 {z := base} default {z := 0}}
+            default {
+                switch mod(n, 2) case 0 { z := base } default { z := x }
+                let half := div(base, 2)  // for rounding.
+                for { n := div(n, 2) } n { n := div(n,2) } {
+                let xx := mul(x, x)
+                if iszero(eq(div(xx, x), x)) { revert(0,0) }
+                let xxRound := add(xx, half)
+                if lt(xxRound, xx) { revert(0,0) }
+                x := div(xxRound, base)
+                if mod(n,2) {
+                    let zx := mul(z, x)
+                    if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+                    let zxRound := add(zx, half)
+                    if lt(zxRound, zx) { revert(0,0) }
+                    z := div(zxRound, base)
+                }
+            }
+            }
+        }
+    }
+
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = mul(x, y) / ONE;
+    }
+
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
     function testDrip() public {
         uint fee = uint(1000000564701133626865910626); // 5 % / day
         pile.file(fee, fee);
@@ -139,36 +181,119 @@ contract PileTest is DSTest {
         assertEq(rho2, now);
         assertEq(debt2, 0);
 
-
         assertTrue(chi1 != chi2);
     }
 
+    function checkDebt(uint loan, uint should) public {
+        (uint debt,,,) = pile.loans(loan);
+        assertEq(debt, should);
+    }
 
-    function testSingleFee() public {
-        uint fee = uint(1000000564701133626865910626); // 5 % / day
+    function checkDebt(uint loan, uint should, uint tolerance) public {
+        (uint debt,,,) = pile.loans(loan);
+        assertEq(debt/tolerance, should/tolerance);
+    }
+
+    function testSingleCompoundSec() public  {
+        /*
+        Compound period in pile is in seconds
+        compound seconds = (1+r/n)^nt
+
+        fee = (1+(r/n))*10^27 (27 digits precise)
+
+        Example:
+        given a 1.05 interest per day (seconds per day 3600 * 24)
+
+        i = (1+r/(3600*24))^(3600*24) would result in r = 0.051271065957324097526787272 per second compound
+
+        fee = (1+(0.051271065957324097526787272/(3600*24)))*10^27
+        fee = 1000000593415115246806684338
+
+        */
+        uint fee  = 1000000593415115246806684338; // 5 % per day compound in seconds
+        uint loan = 1;
+        uint principal = 66 ether;
+        fileFee(fee, loan);
+        borrow(loan, principal);
+
+        // one day later
+        hevm.warp(now + 1 days);
+        pile.collect(loan);
+
+        uint should = calculateDebt(fee, principal,uint(3600*24));
+        checkDebt(loan, should);
+    }
+
+
+    function testSingleCompoundDay() public {
+        /*
+        Compound period in pile is in seconds
+        compound seconds = (1+r/n)^nt
+
+        fee = (1+(r/n))*10^27 (27 digits precise)
+
+        Example: compound in seconds should result in 1.05 interest per day
+
+        given i = 1.05
+        solve equation for r
+        i = (1+r/n)^nt
+        r = n * (i^(1/n)-1
+
+        use calculated r for fee equation
+        fee = (1+((n * (i^(1/n)-1)/n))*10^27
+
+        simplified
+        fee = i^(1/n) * 10^27
+
+        fee = 1.05^(1/(3600*24)) * 10^27 // round 27 digit
+        fee = 1000000564701133626865910626
+
+        */
+
+        uint fee = uint(1000000564701133626865910626); // 5 % day
         pile.file(fee, fee);
         uint loan = 1;
         uint principal = 66 ether;
         pile.file(loan, fee, 0);
         borrow(loan, principal);
-        (uint debt1,,uint fee1 ,uint chi1) = pile.loans(loan);
-        assertEq(debt1, 66 ether);
-        assertEq(fee, fee1);
+
+        checkDebt(loan, 66 ether);
 
         // two days later
         hevm.warp(now + 2 days);
         pile.collect(loan);
 
-        (uint debt2,,uint fee2 ,uint chi2) = pile.loans(loan);
-        assertEq(debt2, 72.765 ether); // 66 ether * 1,05**2
-        assertEq(fee, fee2);
-        assertTrue(chi1 != chi2);
-
+        checkDebt(loan, 72.765 ether);// 66 ether * 1,05**2
     }
 
-    function checkDebt(uint loan, uint wad) public {
-        (uint debt,,,) = pile.loans(loan);
-        assertEq(debt, wad);
+
+    function testSingleCompoundYear() public {
+        /*
+
+        i = 1.12 // 12%
+        n = 24 * 3600 * 365
+
+        simplified fee
+        fee = i^(1/n) * 10^27
+
+        fee = 1.12^(1/(3600*24*365)) * 10^27
+        fee = 1000000003593629043335673583
+        */
+
+        uint fee = uint(1000000003593629043335673583); // 12 % per year
+        pile.file(fee, fee);
+        uint loan = 1;
+        uint principal = 66 ether;
+        pile.file(loan, fee, 0);
+        borrow(loan, principal);
+
+        checkDebt(loan, 66 ether);
+
+        // on year later
+        hevm.warp(now + 365 days);
+        pile.collect(loan);
+
+        checkDebt(loan, 73.92 ether);// 66 ether * 1,12
     }
 
     function testDoubleDripFee() public {
@@ -184,9 +309,9 @@ contract PileTest is DSTest {
 
         (, uint chiF, , ) = pile.fees(fee);
 
-        uint time = now;
-        // day 1
-        hevm.warp(time + 1 days);
+        uint start = now;
+        //  1 day  later
+        hevm.warp(start + 1 days);
         pile.collect(loan);
 
         (,  chiF, , ) = pile.fees(fee);
@@ -195,8 +320,8 @@ contract PileTest is DSTest {
         assertEq(fee, fee2);
         assertTrue(chi1 != chi2);
 
-        // day 2
-        hevm.warp(time + 3 days);
+        // 2 day later
+        hevm.warp(start + 3 days);
         pile.collect(loan);
 
         (,  chiF, , ) = pile.fees(fee);
@@ -204,6 +329,39 @@ contract PileTest is DSTest {
         assertEq(debt3, 76.40325  ether); //  66 ether * 1,05**3
         assertEq(fee, fee3);
         assertTrue(chi2 != chi3);
-
     }
+
+
+    function fileFee(uint fee, uint loan) public {
+        pile.file(fee, fee);
+        pile.file(loan, fee, 0);
+    }
+
+    function calculateDebt(uint fee, uint principal, uint time) internal pure returns(uint z) {
+        z = rmul(principal, rpow(fee,time,ONE));
+    }
+
+    function testFeeDifferentIntervals() public {
+        uint fee = uint(1000000564701133626865910626); // 5 % / daily
+        uint loan = 1;
+        uint principal = 66 ether;
+        fileFee(fee, loan);
+        borrow(loan, principal);
+
+        uint start = now;
+        // half day later
+        hevm.warp(start + 12 hours);
+        pile.collect(loan);
+
+        checkDebt(loan,  calculateDebt(fee, principal,uint(3600*12))); // 67.629875055333349329
+        // 24,5  days later
+        hevm.warp(start + 25 days);
+        pile.collect(loan);
+
+        uint tolerance = 10; // ignore last digit
+        checkDebt(loan,  calculateDebt(fee, principal,uint(3600*24*25)),tolerance);
+        pile.collect(loan); // should have no effect
+        checkDebt(loan,  calculateDebt(fee, principal,uint(3600*24*25)),tolerance);
+    }
+
 }
