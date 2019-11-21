b3960b1e0083149ff2f9a6e93914fd91d947f41c
Charly <charly@centrifuge.io>
2019-11-21 14:58:34 +0100

    wip

---
+++ src/test/mock/lifeguard.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.4.24;
+pragma experimental ABIEncoderV2;
+
+
+contract DeskMock {
+
+    // calls
+    uint public callsBalance;
+    uint public callsGive;
+    uint public callsTake;
+
+    uint public wad;
+
+
+    function balance() public {
+        callsBalance++;
+    }
+
+    function give(uint wad_) public  {
+        wad = wad_;
+        callsGive++;
+    }
+
+    function take(uint wad_) public  {
+        wad = wad_;
+        callsTake++;
+    }
+}
