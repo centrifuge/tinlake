6528ba8bfc21f0e8c38d9150bd4698552a6a74de
Charly <charly@centrifuge.io>
2019-11-22 12:00:57 +0100

    rm unnecessary stuff for initial mock

--- src/test/mock/lifeguard.sol
+++ src/test/mock/lifeguard.sol
@@ -1,27 +1,13 @@
 pragma solidity >=0.4.24;
 
-// Tranche Manager mock
+// Tranche Manager
 contract LifeguardMock {
 
     // calls
     uint public callsBalance;
-    uint public callsGive;
-    uint public callsTake;
-
-    uint public wad;
-
 
     function balance() public {
         callsBalance++;
-    }
-
-    function give(uint wad_) public  {
-        wad = wad_;
-        callsGive++;
-    }
-
-    function take(uint wad_) public  {
-        wad = wad_;
-        callsTake++;
+        // internally triggers Operator
     }
 }
