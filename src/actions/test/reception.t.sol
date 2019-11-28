95c420425ffc4e4e9ea95dc910006bd2342ad9a8
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-11-28 13:35:55 +0100

    own proxy folder

--- src/actions/test/reception.t.sol
+++ src/actions/test/reception.t.sol
@@ -4,12 +4,12 @@ import "ds-test/test.sol";
 
 import "../reception.sol";
 
-import "../../proxy.sol";
-import "../../registry.sol";
+import "../../proxy/proxy.sol";
+import "../../proxy/registry.sol";
 
-import "../../../core/test/mock/shelf.sol";
-import "../../../core/test/mock/pile.sol";
-import "../../../core/test/mock/desk.sol";
+import "../../core/test/mock/shelf.sol";
+import "../../core/test/mock/pile.sol";
+import "../../core/test/mock/desk.sol";
 
 contract RegistryTest is DSTest {
     ProxyRegistry registry;
