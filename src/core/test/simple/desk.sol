bb8adfc37e3760228a586e5054390f5780cea27e
Charly <charly@centrifuge.io>
2019-12-09 16:55:23 +0100

    tranche manager in system test

--- src/core/test/simple/desk.sol
+++ src/core/test/simple/desk.sol
@@ -1,3 +1,18 @@
+// Copyright (C) 2019 Centrifuge
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity >=0.4.24;
 
 contract TokenLike {
@@ -9,13 +24,6 @@ contract PileLike {
     function want() public returns (int);
 }
 
-contract DeskFab {
-    function deploy(address pile_, address token_) public returns (address) {
-        Desk desk = new Desk(pile_, token_);
-        return address(desk);
-    }
-}
-
 contract Desk {
 
     // --- Data ---
@@ -23,27 +31,32 @@ contract Desk {
     // simple tranche manager = 1 tranche/1 operator for now
     TokenLike public token;
     constructor (address pile_, address token_) public {
-        token = TokenLike(token_);
         pile = PileLike(pile_);
+        token = TokenLike(token_);
     }
 
     // --- Calls ---
+
+    function depend(bytes32 what, address addr) public {
+        if (what == "pile") { pile = PileLike(addr); }
+        else if (what == "token") { token = TokenLike(addr); }
+        else revert();
+    }
+
     function balance() public {
         int wad = pile.want();
         if (wad > 0) {
-            give(address(pile), uint(wad));
-
+            take(address(pile), uint(wad));
         } else {
-            take(address(pile), uint(wad*-1));
+            give(address(pile), uint(wad*-1));
         }
     }
 
-    function give(address usr, uint wad) public {
-        token.mint(usr, wad);
+    function take(address pile_, uint wad) public {
+        token.mint(pile_, wad);
     }
 
-    function take(address usr, uint wad) public {
-        token.transferFrom(usr, address(this), wad);
+    function give(address pile_, uint wad) internal {
+        token.transferFrom(pile_, address(this), wad);
     }
-
 }
