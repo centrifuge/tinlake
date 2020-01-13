0033647aa5e384134167a0ab0d0219e120153e13
Charly <charly@centrifuge.io>
2020-01-13 17:12:10 +0100

    operator tests

--- src/core/lender/tranche/test/operator.t.sol
+++ src/core/lender/tranche/test/operator.t.sol
@@ -19,32 +19,78 @@ import "ds-test/test.sol";
 
 import "../../test/mock/tranche.sol";
 import "../../test/mock/assessor.sol";
-import "../operator.sol";
+import "../operator/base.sol";
+import "../operator/allowance.sol";
+import "../operator/whitelist.sol";
 
 contract OperatorTest is DSTest {
 
     AssessorMock assessor;
     TrancheMock tranche;
-    Operator operator;
+    WhitelistOperator whitelist;
+    AllowanceOperator allowance;
+
 
     function setUp() public {
         assessor =  new AssessorMock();
         tranche = new TrancheMock();
-        operator = new Operator(address(tranche), address(assessor));
-        operator.depend("tranche", address(tranche));
+        whitelist = new WhitelistOperator(address(tranche), address(assessor));
+        allowance = new AllowanceOperator(address(tranche), address(assessor));
+        whitelist.depend("tranche", address(tranche));
+        allowance.depend("tranche", address(tranche));
     }
 
-    function testSupply() public {
-        assessor.setReturn("tokenPrice", 1);
-        operator.supply(100);
+    function testWhitelistSupply() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        whitelist.relyInvestor(address(this));
+        whitelist.supply(100 ether);
         assertEq(tranche.calls("supply"), 1);
         assertEq(assessor.calls("tokenPrice"), 1);
     }
 
-    function testRedeem() public {
-        assessor.setReturn("tokenPrice", 1);
-        operator.redeem(100);
+    function testWhitelistRedeem() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        whitelist.relyInvestor(address(this));
+        whitelist.redeem(100 ether);
         assertEq(tranche.calls("redeem"), 1);
         assertEq(assessor.calls("tokenPrice"), 1);
+    }
+
+    function testFailWhitelistSupply() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        whitelist.supply(100 ether);
+    }
+
+    function testFailWhitelistRedeem() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        whitelist.redeem(100 ether);
+    }
+
+    function testAllowanceSupply() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        allowance.approve(address(this), 100 ether, 100 ether);
+        allowance.supply(100 ether);
+        assertEq(tranche.calls("supply"), 1);
+        assertEq(assessor.calls("tokenPrice"), 1);
+    }
+
+    function testAllowanceRedeem() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        allowance.approve(address(this), 100 ether, 100 ether);
+        allowance.redeem(100 ether);
+        assertEq(tranche.calls("redeem"), 1);
+        assertEq(assessor.calls("tokenPrice"), 1);
+    }
+
+    function testFailAllowanceSupply() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        allowance.approve(address(this), 100 ether, 50 ether);
+        allowance.supply(100 ether);
+    }
+
+    function testFailAllowanceRedeem() public {
+        assessor.setReturn("tokenPrice", 1 ether);
+        allowance.approve(address(this), 50 ether, 100 ether);
+        allowance.redeem(100 ether);
     }
 }
