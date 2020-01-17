bb698b7b1fc52a1c8df5863595cab68446173be7
Charly <charly@centrifuge.io>
2020-01-17 14:20:50 +0100

    wip

--- src/core/lender/lenderdeployer.sol
+++ src/core/lender/lenderdeployer.sol
@@ -22,6 +22,7 @@ import {WhitelistOperator} from "./tranche/operator/whitelist.sol";
 import {Tranche} from "./tranche/tranche.sol";
 import {SeniorTranche} from "./tranche/senior_tranche.sol";
 import {Distributor} from "./distributor/switchable.sol";
+import {PricePool} from "../price/pool.sol";
 
 contract TrancheFab {
     function newTranche(address currency, address token) public returns (Tranche tranche) {
@@ -40,15 +41,15 @@ contract SeniorFab {
 }
 
 contract AssessorFab {
-    function newAssessor(address pile) public returns (Assessor assessor) {
-        assessor = new Assessor(pile);
+    function newAssessor(address pool) public returns (Assessor assessor) {
+        assessor = new Assessor(pool);
         assessor.rely(msg.sender);
         assessor.deny(address(this));
     }
 }
 
 contract AllowanceFab {
-    function newAllowanceOperator(address tranche, address assessor) public returns (AllowanceOperator operator) {
+    function newOperator(address tranche, address assessor) public returns (AllowanceOperator operator) {
         operator = new AllowanceOperator(tranche, assessor);
         operator.rely(msg.sender);
         operator.deny(address(this));
@@ -56,7 +57,7 @@ contract AllowanceFab {
 }
 
 contract WhitelistFab {
-    function newWhitelistOperator(address tranche, address assessor) public returns (WhitelistOperator operator) {
+    function newOperator(address tranche, address assessor) public returns (WhitelistOperator operator) {
         operator = new WhitelistOperator(tranche, assessor);
         operator.rely(msg.sender);
         operator.deny(address(this));
@@ -64,12 +65,26 @@ contract WhitelistFab {
 }
 
 contract DistributorFab {
-    function newDistributorFab() public returns (Distributor distributor) {
+    function newDistributor() public returns (Distributor distributor) {
         distributor = new Distributor();
         distributor.rely(msg.sender);
         distributor.deny(address(this));
     }
 }
+
+// pool should be separate from lender deployer, this is just for now
+contract PoolFab {
+    function newPool() public returns (PricePool pool) {
+        pool = new PricePool();
+        pool.rely(msg.sender);
+        pool.deny(address(this));
+    }
+}
+
+contract PileLike {
+    function total() public returns (uint);
+}
+
 
 contract CurrencyLike {
     function balanceOf(address) public returns(uint);
@@ -82,6 +97,8 @@ contract LenderDeployer {
     AllowanceFab allowancefab;
     WhitelistFab whitelistfab;
     DistributorFab distributorfab;
+    // see pool comment above
+    PoolFab poolfab;
 
     address god;
 
@@ -91,9 +108,12 @@ contract LenderDeployer {
     AllowanceOperator public allowance;
     WhitelistOperator public whitelist;
     Distributor public distributor;
+    // see pool comment above
+    PricePool public pool;
+
 
     constructor(address god_, TrancheFab trancheFab_, SeniorFab seniorFab_, AssessorFab assessorFab_, AllowanceFab allowanceFab_,
-    WhitelistFab whitelistFab_, DistributorFab distributorFab_) public {
+    WhitelistFab whitelistFab_, DistributorFab distributorFab_, PoolFab poolFab_) public {
         god = god_;
         tranchefab = trancheFab_;
         seniorfab = seniorFab_;
@@ -101,6 +121,7 @@ contract LenderDeployer {
         allowancefab = allowanceFab_;
         whitelistfab = whitelistFab_;
         distributorfab = distributorFab_;
+        poolfab = poolFab_;
     }
 
     function deployTranche(address currency, address token) public {
@@ -108,7 +129,31 @@ contract LenderDeployer {
         tranche.rely(god);
     }
 
-    function deployAssessor(address pile)
+    function deployPool() public {
+        pool = poolfab.newPool();
+        // pile needs to be added here;
+        pool.rely(god);
+    }
+
+    function deployAssessor(address pool) public {
+        assessor = assessorfab.newAssessor(pool);
+        assessor.rely(god);
+    }
+
+    function deployWhitelistOperator(address tranche, address assessor) public {
+        whitelist = whitelistfab.newOperator(tranche, assessor);
+        whitelist.rely(god);
+    }
+
+    function deployAllowanceOperator(address tranche, address assessor) public {
+        allowance = allowancefab.newOperator(tranche, assessor);
+        allowance.rely(god);
+    }
+
+    function deployDistributor(address currency) public {
+        distributor = distributorfab.newDistributor(currency);
+        distributor.rely(god);
+    }
 
     function elect(){}
 
