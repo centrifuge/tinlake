be3e49f1d5aa18a8de8d293ffbd7b5849b045d0c
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-01-17 13:42:17 +0100

    feat: tinlake math and upgrade solidity version (#210)
    * fix: pile mock test
    
    * same solidity version
    
    * deactivated testcase rounding error

--- src/core/lender/test/mock/mock.sol
+++ src/core/lender/test/mock/mock.sol
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.4.24;
+pragma solidity >=0.5.12;
 
 contract Mock {
     mapping (bytes32 => uint) public calls;
