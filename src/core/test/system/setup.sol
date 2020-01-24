bb9a55a490ce997e010eb305d3b6be475519c612
ilinzweilin <alina@centrifuge.io>
2020-01-24 11:34:10 +0100

    chore: init collector tests

--- src/core/test/system/setup.sol
+++ src/core/test/system/setup.sol
@@ -52,13 +52,6 @@ contract TestSetup {
     TestRootAdmin rootAdmin;
     address rootAdmin_;
 
-    function issueNFT(address usr) public returns (uint tokenId, bytes32 lookupId) {
-        tokenId = collateralNFT.issue(usr);
-        lookupId = keccak256(abi.encodePacked(collateralNFT_, tokenId));
-        return (tokenId, lookupId);
-    }
-
-
     function deployContracts(bytes32 operator, bytes32 distributor_) public {
         collateralNFT = new Title("Collateral NFT", "collateralNFT");
         collateralNFT_ = address(collateralNFT);
