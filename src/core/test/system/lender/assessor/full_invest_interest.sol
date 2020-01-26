65631523f6a779da697696e3f553150d658df3e7
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-01-26 15:08:10 +0100

    test: added distributor system test (#276)
    * assessor edge case fix
    
    * added addtional unit tests
    
    * added distributor tests

--- src/core/test/system/lender/assessor/full_invest_interest.sol
+++ src/core/test/system/lender/assessor/full_invest_interest.sol
@@ -17,7 +17,7 @@ pragma solidity >=0.5.12;
 
 import "../../system.t.sol";
 
-contract SupplyTwoTrancheTest is BaseSystemTest {
+contract FullInterestAssessorTest is BaseSystemTest {
 
     Hevm hevm;
 
@@ -26,7 +26,7 @@ contract SupplyTwoTrancheTest is BaseSystemTest {
     function setUp() public {
         bytes32 operator_ = "whitelist";
         bytes32 distributor_ = "default";
-        bytes32 assessor_ = "default";
+        bytes32 assessor_ = "full_investment";
         bool deploySeniorTranche = true;
         baseSetup(operator_, distributor_,assessor_, deploySeniorTranche);
 
@@ -59,7 +59,7 @@ contract SupplyTwoTrancheTest is BaseSystemTest {
         supplySenior(seniorInvestorAmount);
         supplyJunior(juniorInvestorAmount);
 
-        // currency equals token amount
+        // currency eqals token amount
         seniorInvestor.doRedeem(seniorInvestorAmount);
         juniorInvestor.doRedeem(juniorInvestorAmount);
 
@@ -68,71 +68,63 @@ contract SupplyTwoTrancheTest is BaseSystemTest {
     function testSeniorInterest() public {
         // interest per day of senior tranche is 5%
         uint amount = 100 ether;
-
-        // total in tranches: 200 ether
         supplySenior(amount);
-        supplyJunior(amount);
 
-        // case no interest: no loans borrowed
         assertEq(senior.debt(), 0 ether);
-        supplySenior(amount);
         hevm.warp(now + 1 days);
         senior.drip();
-        assertEq(senior.interest(), 0 ether);
 
-        // case no interest: only junior borrowed
-        (uint loan, ) = createLoanAndWithdraw(borrower_, 80 ether);
-        assertEq(senior.borrowed(), 0 ether);
-
+        assertEq(senior.interest(), 5 ether);
         hevm.warp(now + 1 days);
         senior.drip();
-        assertEq(senior.interest(), 0 ether);
+        assertEq(senior.interest(), 10.25 ether);
+        assertEq(senior.borrowed(), 0);
 
-        // case make interest: on senior debt of 10 ether
-        createLoanAndWithdraw(borrower_, 30 ether);
-        assertEq(senior.borrowed(), 10 ether);
+        // additional investment
+        uint secondAmount = 100 ether;
+        currency.mint(seniorInvestor_, secondAmount);
+        seniorInvestor.doSupply(secondAmount);
 
-        hevm.warp(now + 1 days);
-        senior.drip();
-        // interest on 10 ether borrowed
-        assertEq(senior.interest(), 0.5 ether); // 10 * 1.05 = 10.5 ether
-
-        assertEq(senior.debt(), 10.5 ether);
-        // partial borrow of first loan
-        // first interest
-        repayLoan(borrower_, loan, 0.5 ether);
-        assertEq(senior.interest(), 0 ether);
-        assertEq(senior.debt(), 10 ether);
-        // partial repay to reduce senior debt to 0
-        repayLoan(borrower_, loan, 10 ether);
-        assertEq(senior.debt(), 0 ether);
+        // interest should stay the same
+        assertEq(senior.interest(), 10.25 ether);
+        assertEq(senior.borrowed(), 0);
 
         hevm.warp(now + 1 days);
+
+        assertEq(senior.balance(), 200 ether);
+        assertEq(senior.interest(), 10.25 ether);
+        assertEq(senior.borrowed(), 0);
+
+        // interestBearingAmount 210.25 * 1.05 = 220.7625
+        // delta interest: 5.2625
+        uint interestDelta = assessor.accrueTrancheInterest(address(senior));
+        assertEq(interestDelta, 10.5125 ether);
+
         senior.drip();
-        // no new interest for senior
-        assertEq(senior.interest(), 0 ether);
+        assertEq(senior.balance(), 200 ether);
+        assertEq(senior.borrowed(), 0);
+
+        assertEq(senior.interest(), 10.25 ether + interestDelta);
 
     }
 
     function testRedeemInvestmentWithInterest() public {
+        // interest per day of senior tranche is 5%
         uint amount = 100 ether;
-        uint total = 200 ether;
-
-        // total in tranches: 200 ether
         supplySenior(amount);
         supplyJunior(amount);
 
-        // + 5 ether interest for senior
-        (uint loan, ) = createLoanAndWithdraw(borrower_, total);
-        assertEq(senior.borrowed(), 100 ether);
+        // no loans senior will get currency from junior
         hevm.warp(now + 1 days);
-        repayLoan(borrower_, loan, total);
-        assertEq(senior.balance(), amount + 5 ether);
+        senior.drip();
+        assertEq(senior.interest(), 5 ether);
+
 
         // senior: 100 token for 105 currency
         uint seniorToken = 100 ether;
         seniorInvestor.doRedeem(seniorToken);
         assertEq(currency.balanceOf(seniorInvestor_), 105 ether);
+
 
         // junior 100 token for 95 currency
         uint juniorToken = 100 ether;
@@ -140,4 +132,35 @@ contract SupplyTwoTrancheTest is BaseSystemTest {
         assertEq(currency.balanceOf(juniorInvestor_), 95 ether);
     }
 
+    function testTrancheInterestWithLoans() public {
+        // interest per day of senior tranche is 5%
+        uint amount = 100 ether;
+        supplySenior(amount);
+
+        assertEq(senior.debt(), 0 ether);
+
+        // loan borrowed (only senior has currency)
+        // should have no impact on interest calc
+       (uint loan, ) = createLoanAndWithdraw(borrower_, 80 ether);
+        assertEq(senior.borrowed(), 80 ether);
+        assertEq(senior.balance(), 20 ether);
+
+        // one day later
+        hevm.warp(now + 1 days);
+        senior.drip();
+
+        assertEq(senior.interest(), 5 ether);
+
+        // repayment
+        repayLoan(borrower_, loan, 80 ether);
+        assertEq(senior.balance(), 100 ether);
+        // interest is repaid first
+        assertEq(senior.interest(), 0 ether);
+        assertEq(senior.borrowed(), 5 ether);
+
+        hevm.warp(now + 1 days);
+        senior.drip();
+        assertEq(senior.interest(), 5.25 ether);
+
+    }
 }
