fb4369db862703b19a6468958f81bbcd50a02b65
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-02-05 15:39:22 +0100

    fix: governance address

---
+++ bin/README.md
@@ -0,0 +1,26 @@
+# Tinlake Deployment
+The Tinlake Deployment happens with bash script and the help of seth.
+
+
+## Deploy Config File
+The Tinlake depl
+
+### Required
+```json
+{
+  "ETH_RPC_URL": "<<RPC URL>>",
+  "ETH_FROM": "<<ADDRESS>>",
+  "ETH_GAS": "7000000",
+  "ETH_KEYSTORE": "<<DIR PATH>>",
+  "ETH_PASSWORD": "<<FILE PATH>>",
+  "TINLAKE_CURRENCY": "<<ADDRESS>>"
+}
+```
+
+### Optional
+
+
+
+config_unknown.json
+### Auto Generate Config File 
+For local deployments 
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,14 +25,26 @@ source ./root/deploy.sh
 # deploy lender contracts
 source ./lender/deploy.sh
 
-## deploy borrower contracts
-#source ./borrower/deploy.sh
-#
-## finalize deployment
-#
-#GOVERNANCE=$ETH_FROM
-#seth send $ROOT_CONTRACT 'prepare(address,address,address)'$LENDER_DEPLOYER $BORROWER_DEPLOYER $GOVERNANCE
-#seth send $ROOT_CONTRACT 'deploy()'
-#
-#success_msg "Tinlake Deployment Finished"
-#success_msg "Deployment File: $(realpath $DEPLOYMENT_FILE)"
+# deploy borrower contracts
+source ./borrower/deploy.sh
+
+# finalize deployment
+message Finalize Deployment
+GOVERNANCE="0x$ETH_FROM"
+
+seth send $ROOT_CONTRACT 'prepare(address,address,address)' $LENDER_DEPLOYER $BORROWER_DEPLOYER $GOVERNANCE
+seth send $ROOT_CONTRACT 'deploy()'
+
+success_msg "Tinlake Deployment Finished"
+success_msg "Deployment File: $(realpath $DEPLOYMENT_FILE)"
+
+#touch $DEPLOYMENT_FILE
+addValuesToFile $DEPLOYMENT_FILE <<EOF
+{
+    "GOVERNANCE" : "$GOVERNANCE"
+}
+EOF
+
+cat $DEPLOYMENT_FILE
+
+success_msg DONE
