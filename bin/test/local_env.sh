b893c665424a05d6ce6cc12d0516fbf75be2915c
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-02-07 13:11:57 +0100

    added pile address and better deploy scripts

---
+++ bin/test/local_env.sh
@@ -0,0 +1,11 @@
+# Define ENV
+GETH_DIR=$HOME/.dapp/testnet/8545/
+
+# Default Test Config
+export ETH_RPC_URL=http://127.0.0.1:8545
+export ETH_KEYSTORE=$GETH_DIR/keystore
+touch $GETH_DIR/.empty-password
+export ETH_PASSWORD=$GETH_DIR/.empty-password
+export ETH_FROM=$(cat $GETH_DIR/keystore/* | jq -r '.address' | head -n 1)
+
+export ETH_GAS=${ETH_GAS:-"7000000"}
