f91c8dc5bcf3dab9afd0b04b7aae3a2df478b927
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-05-01 02:54:14 +0200

    optimized require in supply

--- src/lender/tranche/test/proportional_operator.t.sol
+++ src/lender/tranche/test/proportional_operator.t.sol
@@ -112,8 +112,8 @@ contract ProportionalOperatorTest is DSTest, Math {
     }
 
     function setUpInvestors(uint amountA, uint amountB) public returns(uint) {
-        supplyInvestor(investorA, 100 ether);
-        supplyInvestor(investorB, 100 ether);
+        supplyInvestor(investorA, amountA);
+        supplyInvestor(investorB, amountB);
 
         uint totalSupply = amountA + amountB;
         tranche.setReturn("tokenSupply", totalSupply);
@@ -192,89 +192,6 @@ contract ProportionalOperatorTest is DSTest, Math {
         investor.doRedeem(address(operator), tokenAmount);
         assertEq(tranche.values_uint("redeem_currencyAmount"), expectedCurrencyAmount);
         assertEq(tranche.values_uint("redeem_tokenAmount"), tokenAmount);
-    }
-
-    uint constant loanAmount = 3;
-
-    function runScenario(uint amountInvestorA, uint amountInvestorB, uint[loanAmount] memory principalReturned, uint[loanAmount] memory currencyReturned,
-        uint[loanAmount] memory maxToken, uint[loanAmount] memory tokenAmount, uint[loanAmount] memory expectedCurrency, uint maxTokenB,uint tokenAmountB, uint expectedReturnB ) public {
-
-        setUpInvestors(amountInvestorA, amountInvestorB);
-
-        // investor A
-        uint totalInvestorA = 0;
-        for(uint i = 0; i < loanAmount; i++) {
-            // simulate loan repayment
-            operator.updateReturned(currencyReturned[i], principalReturned[i]);
-            redeem(investorA, maxToken[i], tokenAmount[i], expectedCurrency[i]);
-            totalInvestorA += expectedCurrency[i];
-        }
-
-        // investor B only redeems once after loan 3
-        redeem(investorB, maxTokenB, tokenAmountB, expectedReturnB);
-
-        // both investors should have the same amount of tokens in the end
-        // 1 wei tolerance
-        assertTrue(expectedReturnB-totalInvestorA <= 1);
-    }
-
-    function testScenarioRedeemA() public {
-        /*
-
-        Scenario Description:
-                    currencyAmount   tokenAmount
-        Investor A: $100                 100
-        Investor B: $100                 100
-        Total:      $200                 200
-
-        Loans
-
-        +---------+-----------+-----------+-------------------+-------------+
-        | Loan    | Principal | Repayment | % Principal       | Desc        |
-        +---------+-----------+-----------+-------------------+-------------+
-        | Loan 1  | 90        | 101       | 0.45              | profit: 11  |
-        +---------+-----------+-----------+-------------------+-------------+
-        | Loan 2  | 40        | 44        | 0.2               | profit: 4   |
-        +---------+-----------+-----------+-------------------+-------------+
-        | Loan 3  | 70        | 65        | 0.35              | loss: 5     |
-        +---------+-----------+-----------+-------------------+-------------+
-
-        Total Profit: 10
-        Investor A: 105
-        Investor B: 105
-
-        Investor A
-	                    max 	tokenAmount     currencyAmount
-        After Loan 1	45.00	    30.00	        $33.67
-        After loan 2 	35.00	    15.00	        $16.64
-        After Loan 3	55.00	    55.00	    	$54.69
-        Total:                                      $105
-
-
-        Investor B
-                            max 	tokenAmount     currencyAmount
-        After Loan 3    	100.00	    100.00	        $105
-        */
-
-        uint[loanAmount] memory principalReturned = [90 ether , uint(40 ether), uint(70 ether)];
-        uint[loanAmount] memory currencyReturned = [101 ether, uint(44 ether), uint(65 ether)];
-
-        // investor A
-        uint[loanAmount] memory maxToken = [45 ether ,uint(35000000000000000000), uint(55000000000000000000)];
-        uint[loanAmount] memory tokenAmount = [30 ether ,uint(15 ether), uint(55000000000000000000)];
-        uint[loanAmount] memory expectedCurrency = [33666666666666666666, uint(16642857142857142857), uint(54690476190476190476)];
-
-        uint amountInvestorA = 100 ether;
-        uint amountInvestorB = 100 ether;
-
-        // investor B only redeems once after loan 3
-        uint maxTokenB = 100 ether;
-        uint tokenAmountB = 100 ether;
-        uint expectedReturnB = 105 ether;
-
-        runScenario(amountInvestorA, amountInvestorB, principalReturned, currencyReturned, maxToken, tokenAmount,
-            expectedCurrency, maxTokenB, tokenAmountB, expectedReturnB);
-
     }
 
     function testNotSupplyAll() public {
@@ -356,6 +273,148 @@ contract ProportionalOperatorTest is DSTest, Math {
         investorC.doRedeem(address(operator), tokenAmount);
         assertEq(tranche.values_uint("redeem_currencyAmount"), expectedCurrencyAmount);
         assertEq(tranche.values_uint("redeem_tokenAmount"), tokenAmount);
+    }
+
+    uint constant loanAmount = 3;
+    function runScenario(uint amountInvestorA, uint amountInvestorB, uint[loanAmount] memory principalReturned, uint[loanAmount] memory currencyReturned,
+        uint[loanAmount] memory maxToken, uint[loanAmount] memory tokenAmount, uint[loanAmount] memory expectedCurrency, uint maxTokenB,uint tokenAmountB, uint expectedReturnB ) public returns(uint, uint) {
+
+        setUpInvestors(amountInvestorA, amountInvestorB);
+
+        // investor A
+        uint totalInvestorA = 0;
+        for(uint i = 0; i < loanAmount; i++) {
+            // simulate loan repayment
+            operator.updateReturned(currencyReturned[i], principalReturned[i]);
+            redeem(investorA, maxToken[i], tokenAmount[i], expectedCurrency[i]);
+            totalInvestorA += expectedCurrency[i];
+        }
+
+        // investor B only redeems once after loan 3
+        redeem(investorB, maxTokenB, tokenAmountB, expectedReturnB);
+
+        emit log_named_uint("total investor A", totalInvestorA);
+
+        return (totalInvestorA, expectedReturnB);
+    }
+
+    function testScenarioRedeemA() public {
+        /*
+
+        Scenario Description:
+                    currencyAmount   tokenAmount
+        Investor A: $100                 100
+        Investor B: $100                 100
+        Total:      $200                 200
+
+        Loans
+
+        +---------+-----------+-----------+-------------------+-------------+
+        | Loan    | Principal | Repayment | % Principal       | Desc        |
+        +---------+-----------+-----------+-------------------+-------------+
+        | Loan 1  | 90        | 101       | 0.45              | profit: 11  |
+        +---------+-----------+-----------+-------------------+-------------+
+        | Loan 2  | 40        | 44        | 0.2               | profit: 4   |
+        +---------+-----------+-----------+-------------------+-------------+
+        | Loan 3  | 70        | 65        | 0.35              | loss: 5     |
+        +---------+-----------+-----------+-------------------+-------------+
+
+        Total Profit: 10
+        Investor A: 105
+        Investor B: 105
+
+        Investor A
+	                    max 	tokenAmount     currencyAmount
+        After Loan 1	45.00	    30.00	        $33.67
+        After loan 2 	35.00	    15.00	        $16.64
+        After Loan 3	55.00	    55.00	    	$54.69
+        Total:                                      $105
+
+
+        Investor B
+                            max 	tokenAmount     currencyAmount
+        After Loan 3    	100.00	    100.00	        $105
+        */
+
+        uint[loanAmount] memory principalReturned = [90 ether , uint(40 ether), uint(70 ether)];
+        uint[loanAmount] memory currencyReturned = [101 ether, uint(44 ether), uint(65 ether)];
+
+        // investor A
+        uint[loanAmount] memory maxToken = [45 ether ,uint(35000000000000000000), uint(55000000000000000000)];
+        uint[loanAmount] memory tokenAmount = [30 ether ,uint(15 ether), uint(55000000000000000000)];
+        uint[loanAmount] memory expectedCurrency = [33666666666666666666, uint(16642857142857142857), uint(54690476190476190476)];
+
+        uint amountInvestorA = 100 ether;
+        uint amountInvestorB = 100 ether;
+
+        // investor B only redeems once after loan 3
+        uint maxTokenB = 100 ether;
+        uint tokenAmountB = 100 ether;
+        uint expectedReturnB = 105 ether;
+
+        (uint totalA, uint totalB) = runScenario(amountInvestorA, amountInvestorB, principalReturned, currencyReturned, maxToken, tokenAmount,
+            expectedCurrency, maxTokenB, tokenAmountB, expectedReturnB);
+
+        // both investors should have the same amount of tokens in the end
+        // 1 wei tolerance
+        assertTrue(totalB-totalA <= 1);
+    }
+
+    function testScenarioRedeemB() public {
+        /*
+
+        Scenario Description:
+                    currencyAmount   tokenAmount
+        Investor A: $110                 110
+        Investor B: $90                  90
+        Total:      $200                 200
+
+        Loans
+
+        +---------+-----------+-----------+
+        | Loan    | Principal | Repayment |
+        +---------+-----------+-----------+
+        | Loan 1  | $90.00    | 120       |
+        +---------+-----------+-----------+
+        | Loan 2  | $50.00    | 50        |
+        +---------+-----------+-----------+
+        | Loan 3  | $60.00    | 90        |
+        +---------+-----------+-----------+
+        Total:      $200.00     $260.00
+
+        Investor A
+	    Drop max redeem	Drop Redeem amount	tokenPrice (drop)	DAI returned
+        After Loan 1	49.50	36.00	1.33333	$48.00
+        After loan 2 	41.00	15.00	1.10976	$16.65
+        After Loan 3	59.00	59.00	1.32803	$78.35
+        Total		110.00		$143.00
+
+        Investor B
+        After Loan 3	Drop max redeem	Drop Redeem	tokenPrice	DAI Returned
+	    90.00	90.00	$1.30	$117.00
+        */
+
+        uint amountInvestorA = 110 ether;
+        uint amountInvestorB = 90 ether;
+
+        uint[loanAmount] memory principalReturned = [90 ether , uint(50 ether), uint(60 ether)];
+        uint[loanAmount] memory currencyReturned = [120 ether, uint(50 ether), uint(90 ether)];
+
+
+        // investor A
+        uint[loanAmount] memory maxToken = [49.5 ether ,uint(41 ether), uint(59 ether)];
+        uint[loanAmount] memory tokenAmount = [36 ether ,uint(15 ether), uint(59 ether)];
+        uint[loanAmount] memory expectedCurrency = [uint(47999999999999999999), uint(16646341463414634146), uint(78353658536585365853)];
+
+        // investor B only redeems once after loan 3
+        uint maxTokenB = 90 ether;
+        uint tokenAmountB = 90 ether;
+        uint expectedReturnB = 117 ether;
+
+        (uint totalA, uint totalB) = runScenario(amountInvestorA, amountInvestorB, principalReturned, currencyReturned, maxToken, tokenAmount,
+            expectedCurrency, maxTokenB, tokenAmountB, expectedReturnB);
+
+        assertEq(totalA, uint(142999999999999999999));
     }
 }
 
