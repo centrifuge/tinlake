bcfabb97a89fcf5f9df0b10e7bdb83746c423a0c
Alina Sinelnikova <sinelnikovaaa@gmail.com>
2020-09-08 18:05:00 +0200

    chore - add comments to nav (#439)
    * dapp init Tinlake
    
    * dapp upgrade tinlake-nftfeed
    
    * feature- close borrows during submission
    
    * chore: remove unused code
    
    * feature: migrate nftFeed
    
    * fix: scenario tests
    
    * fix: remove unused code
    
    * remove unused code
    
    * feature: add restricted token contract
    
    * feature: integrate restricted token into test setup
    
    * feature: add membership validity to token restriction
    
    * fix: remove dstest
    
    * fix: remove ds-test
    
    * fix: remove ds-test
    
    * fix: formatting
    
    * fix: formatting
    
    * remove logs
    
    * remove logs
    
    * fix: remove unused code
    
    * fix: remove unused code
    
    * fix: remove zip
    
    * fix: recover higher then loan debt
    
    * fix: remove changes from pie
    
    * chore/add-nft-feed-tests
    
    * fix: tests
    
    * chore: add docs to nav feed
    
    * fix: wording
    
    Co-authored-by: ilinzweilin <alina@centrifuge.io>

--- src/borrower/feed/navfeed.sol
+++ src/borrower/feed/navfeed.sol
@@ -21,13 +21,22 @@ import "./nftfeed.sol";
 import "./buckets.sol";
 import "../../fixed_point.sol";
 
+// The Nav Feed contract extends the functionality of the NFT Feed by the Net Asset Value (NAV) computation of a Tinlake pool.
+// NAV is computed as the sum of all discounted future values (fv) of ongoing loans (debt > 0) in the pool.
+// The applied discountRate is dependant on the maturity data of the underlying collateral. The discount decreases with the maturity date approaching.
+// To optimize the NAV calculation the discounting of future values happens bucketwise. FVs from assets with the same maturity date are added to one bucket. 
+// This safes iterations & gas, as the same discountRates can be applied per bucket. 
 contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
-    // nftID => maturityDate
+
+    // maturityDate is the expected date of repayment for an asset
+    // nftID => maturityDate 
     mapping (bytes32 => uint) public maturityDate;
 
+    // recoveryRatePD is a combined rate that includes the probability of default for an asset of a certain risk group and its recovery rate
     // risk => recoveryRatePD
     mapping (uint => Fixed27) public recoveryRatePD;
 
+    // futureValue of an asset based on the loan debt, interest rate, maturity date and recoveryRatePD
     // nftID => futureValue
     mapping (bytes32 => uint) public futureValue;
 
@@ -39,9 +48,12 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         Fixed27 percentage;
     }
 
+    // discount rate applied on every asset's fv depending on its maturityDate. The discount decreases with the maturityDate approaching.
     Fixed27 public discountRate;
 
-    // approximated NAV
+    // approximatedNAV is calculated in case of borrows & repayments between epoch executions. 
+    // It decreases/increases the NAV by the repaid/borrowed amount without running the NAV calculation routine.
+    // This is required for more accurate Senior & JuniorAssetValue estimations between epochs
     uint public approximatedNAV;
 
     uint constant public  WRITE_OFF_PHASE_A = 91;
@@ -56,21 +68,19 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         // gas optimized initialization of writeOffs
         // write off are hardcoded in the contract instead of init function params
 
-        // risk group recoveryRatePD
+        // rist groups are extended by the recoveryRatePD parameter
         recoveryRatePD[0] = Fixed27(ONE);
         recoveryRatePD[1] = Fixed27(90 * 10**25);
-
         // todo change to != 0, breaks currently some system tests
         recoveryRatePD[2] = Fixed27(0);
         recoveryRatePD[3] = Fixed27(ONE);
         recoveryRatePD[4] = Fixed27(ONE);
 
-        // sample write off phases
+        /// Overdue loans (= loans that were not repaid by the maturityDate) are moved to write Offs
         // 60% -> 40% write off
         writeOffs[0] = WriteOff(WRITE_OFF_PHASE_A, Fixed27(6 * 10**26));
         // 80% -> 20% write off
         writeOffs[1] = WriteOff(WRITE_OFF_PHASE_B, Fixed27(8 * 10**26));
-
     }
 
     function uniqueDayTimestamp(uint timestamp) public pure returns (uint) {
@@ -90,38 +100,45 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         } else { revert("unknown config parameter");}
     }
 
-    /// Ceiling Implementation
+    // In case of successful borrow the approximatedNAV is increased by the borrowed amount
     function borrow(uint loan, uint amount) external auth returns(uint navIncrease) {
         uint navIncrease = _borrow(loan, amount);
         approximatedNAV = safeAdd(approximatedNAV, navIncrease);
         return navIncrease;
     }
 
+
+    // On borrow: the discounted future value of the asset is computed based on the loan amount and addeed to the bucket with the according maturity Date
     function _borrow(uint loan, uint amount) internal returns(uint navIncrease) {
         // ceiling check uses existing loan debt
         require(ceiling(loan) >= safeAdd(borrowed[loan], amount), "borrow-amount-too-high");
 
         bytes32 nftID_ = nftID(loan);
         uint maturityDate_ = maturityDate[nftID_];
+        // maturity date has to be a value in the future 
         require(maturityDate_ > block.timestamp, "maturity-date-is-not-in-the-future");
 
         // calculate future value FV
         uint fv = calcFutureValue(loan, amount, maturityDate_, recoveryRatePD[risk[nftID_]].value);
         futureValue[nftID_] = safeAdd(futureValue[nftID_], fv);
 
+        // add future value to the bucket of assets with the same maturity date
         if (buckets[maturityDate_].value == 0) {
             addBucket(maturityDate_, fv);
         } else {
             buckets[maturityDate_].value = safeAdd(buckets[maturityDate_].value, fv);
         }
 
+        // increase borrowed amount for future ceiling computations
         borrowed[loan] = safeAdd(borrowed[loan], amount);
 
         // return increase NAV amount
         return calcDiscount(fv, uniqueDayTimestamp(block.timestamp), maturityDate_);
     }
 
+    // calculate the future value based on the amount, maturityDate interestRate and recoveryRate 
     function calcFutureValue(uint loan, uint amount, uint maturityDate_, uint recoveryRatePD_) public returns(uint) {
+        // retrieve interest rate from the pile
         (, ,uint loanInterestRate, ,) = pile.rates(pile.loanRates(loan));
         return rmul(rmul(rpow(loanInterestRate,  safeSub(maturityDate_, uniqueDayTimestamp(now)), ONE), amount), recoveryRatePD_);
     }
@@ -135,6 +152,7 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
             return;
         }
 
+        // nfts can only be added to risk groups that are part of the score card
         require(thresholdRatio[risk_] != 0, "risk group not defined in contract");
         risk[nftID_] = risk_;
 
@@ -146,13 +164,16 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         uint loan = shelf.nftlookup(nftID_);
         uint maturityDate_ = maturityDate[nftID_];
 
-        // calculate future value FV
+        // Changing the risk group of an nft, might lead to a new interest rate for the dependant loan.
+        // New interest rate leads to a future value.
+        // recalculation required
         buckets[maturityDate_].value = safeSub(buckets[maturityDate_].value, futureValue[nftID_]);
 
         futureValue[nftID_] = calcFutureValue(loan, pile.debt(loan), maturityDate[nftID_], recoveryRatePD[risk[nftID_]].value);
         buckets[maturityDate_].value = safeAdd(buckets[maturityDate_].value, futureValue[nftID_]);
     }
 
+    // In case of successful repayment the approximatedNAV is decreased by the repaid amount
     function repay(uint loan, uint amount) external auth returns (uint navDecrease) {
         uint navDecrease = _repay(loan, amount);
         if (navDecrease > approximatedNAV) {
@@ -167,6 +188,7 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         return navDecrease;
     }
 
+    // On repayment: adjust future value bucket according to repayment amount
     function _repay(uint loan, uint amount) internal returns (uint navDecrease) {
         bytes32 nftID_ = nftID(loan);
         uint maturityDate_ = maturityDate[nftID_];
@@ -175,19 +197,19 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
         buckets[maturityDate_].value = safeSub(buckets[maturityDate_].value, futureValue[nftID_]);
 
         uint debt = pile.debt(loan);
-
         debt = safeSub(debt, amount);
 
         uint fv = 0;
         uint preFutureValue = futureValue[nftID_];
 
+        // in case of partial repayment, compute the fv of the remaining debt and add to the according fv bucket 
         if (debt != 0) {
-            // calculate new future value for loan if debt is still existing
             fv = calcFutureValue(loan, debt, maturityDate_, recoveryRatePD[risk[nftID_]].value);
             buckets[maturityDate_].value = safeAdd(buckets[maturityDate_].value, fv);
             futureValue[nftID_] = fv;
         }
 
+        // remove buckets if no remaining assets
         if (buckets[maturityDate_].value == 0 && firstBucket != 0) {
             removeBucket(maturityDate_);
         }
@@ -205,6 +227,7 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
     function calcDiscount(uint amount, uint normalizedBlockTimestamp, uint maturityDate) public view returns (uint result) {
         return rdiv(amount, rpow(discountRate.value, safeSub(maturityDate, normalizedBlockTimestamp), ONE));
     }
+
 
     function calcTotalDiscount() public view returns(uint) {
         uint normalizedBlockTimestamp = uniqueDayTimestamp(block.timestamp);
@@ -230,7 +253,7 @@ contract NAVFeed is BaseNFTFeed, Interest, Buckets, FixedPoint {
     function currentNAV() public view returns(uint) {
         uint nav_ = calcTotalDiscount();
 
-        // add write offs to NAV
+        // include ovedue assets to the current NAV calculation
         for (uint i = 0; i < writeOffs.length; i++) {
             (uint pie, uint chi, , ,) = pile.rates(writeOffs[i].rateGroup);
             nav_ = safeAdd(nav_, rmul(rmul(pie, chi), writeOffs[i].percentage.value));
