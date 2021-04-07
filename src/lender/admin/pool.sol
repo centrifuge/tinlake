34f40b4550103b7469b8a3d47ffeddb4a3d6b2f2
Jeroen Offerijns <jeroen@offerijns.com>
2021-04-07 21:54:49 +0200

    feat: add events to pool admin

--- src/lender/admin/pool.sol
+++ src/lender/admin/pool.sol
@@ -25,9 +25,9 @@ interface LendingAdapter {
     function sink(uint256 amount) external;
 }
 
-contract MemberlistLike {
-    function updateMember(address usr, uint256 validUntil) public;
-    function updateMembers(address[] memory users, uint256 validUntil) public;
+interface MemberlistLike {
+    function updateMember(address usr, uint256 validUntil) external;
+    function updateMembers(address[] calldata users, uint256 validUntil) external;
 }
 
 // Wrapper contract for various pool management tasks.
@@ -42,6 +42,18 @@ contract PoolAdmin is Auth {
 
     // Admins can manage pools, but have to be added and can be removed by any ward on the PoolAdmin contract
     mapping(address => uint256) public admins;
+
+    // Events
+    event File(bytes32 indexed what, bool indexed data);
+    event RelyAdmin(address indexed usr);
+    event DenyAdmin(address indexed usr);
+    event SetMaxReserve(uint256 value);
+    event RaiseCreditline(uint256 amount);
+    event SinkCreditline(uint256 amount);
+    event UpdateSeniorMember(address indexed usr, uint256 validUntil);
+    event UpdateSeniorMembers(address[] indexed users, uint256 validUntil);
+    event UpdateJuniorMember(address indexed usr, uint256 validUntil);
+    event UpdateJuniorMembers(address[] indexed users, uint256 validUntil);
 
     constructor() public {
         wards[msg.sender] = 1;
@@ -61,47 +73,57 @@ contract PoolAdmin is Auth {
 
     function file(bytes32 what, bool data) public auth {
         live = data;
+        emit File(what, data);
     }
 
     modifier admin { require(admins[msg.sender] == 1 && live); _; }
 
-    function relyAdmin(address usr) public auth note {
+    function relyAdmin(address usr) public auth {
         admins[usr] = 1;
+        emit RelyAdmin(usr);
     }
 
-    function denyAdmin(address usr) public auth note {
+    function denyAdmin(address usr) public auth {
         admins[usr] = 0;
+        emit DenyAdmin(usr);
     }
 
     // Manage max reserve
     function setMaxReserve(uint256 value) public admin {
         assessor.file("maxReserve", value);
+        emit SetMaxReserve(value);
     }
 
     // Manage creditline
     function raiseCreditline(uint256 amount) public admin {
         lending.raise(amount);
+        emit RaiseCreditline(amount);
     }
 
     function sinkCreditline(uint256 amount) public admin {
         lending.sink(amount);
+        emit SinkCreditline(amount);
     }
 
     // Manage memberlists
     function updateSeniorMember(address usr, uint256 validUntil) public admin {
         seniorMemberlist.updateMember(usr, validUntil);
+        emit UpdateSeniorMember(usr, validUntil);
     }
 
     function updateSeniorMembers(address[] memory users, uint256 validUntil) public admin {
         seniorMemberlist.updateMembers(users, validUntil);
+        emit UpdateSeniorMembers(users, validUntil);
     }
 
     function updateJuniorMember(address usr, uint256 validUntil) public admin {
         juniorMemberlist.updateMember(usr, validUntil);
+        emit UpdateJuniorMember(usr, validUntil);
     }
 
     function updateJuniorMembers(address[] memory users, uint256 validUntil) public admin {
         juniorMemberlist.updateMembers(users, validUntil);
+        emit UpdateJuniorMembers(users, validUntil);
     }
     
 }
