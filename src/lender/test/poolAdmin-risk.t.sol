20dffc1d1ae541b96402c329d40b5613b92d9c02
Adam Stockermans <adam@centrifuge.io>
2021-09-27 10:13:31 -0400

    Add NAVFeed contract instead of just the mock

--- src/lender/test/poolAdmin-risk.t.sol
+++ src/lender/test/poolAdmin-risk.t.sol
@@ -5,6 +5,7 @@ import "ds-test/test.sol";
 
 import "./../assessor.sol";
 import "./../admin/pool.sol";
+import "./../../borrower/feed/navfeed.sol";
 import "./mock/coordinator.sol";
 import "./mock/navFeed.sol";
 import "./mock/memberlist.sol";
@@ -19,7 +20,8 @@ contract RiskManagementPoolAdminTest is DSTest {
     MemberlistMock seniorMemberlist;
     MemberlistMock juniorMemberlist;
     CoordinatorMock coordinator;
-    NAVFeedMock navFeed;
+    NAVFeedMock navFeedMock;
+    NAVFeed navFeed;
     PoolAdmin poolAdmin;
 
     function setUp() public {
@@ -28,7 +30,7 @@ contract RiskManagementPoolAdminTest is DSTest {
         seniorMemberlist = new MemberlistMock();
         juniorMemberlist = new MemberlistMock();
         coordinator = new CoordinatorMock();
-        navFeed = new NAVFeedMock();
+        // navFeed = new NAVFeed();
         poolAdmin = new PoolAdmin();
 
         assessor.rely(address(poolAdmin));
@@ -50,8 +52,8 @@ contract RiskManagementPoolAdminTest is DSTest {
         uint loan = 1;
         uint index = 0;
         poolAdmin.overrideWriteOff(loan, index);
-        assertEq(navFeed.values_uint("overrideWriteOff_loan"), loan);
-        assertEq(navFeed.values_uint("overrideWriteOff_index"), index);
+        assertEq(navFeedMock.values_uint("overrideWriteOff_loan"), loan);
+        assertEq(navFeedMock.values_uint("overrideWriteOff_index"), index);
     }
 
     function testOverrideWriteOff() public {
@@ -65,7 +67,9 @@ contract RiskManagementPoolAdminTest is DSTest {
 
     function callAddRiskGroup() public {
         poolAdmin.addRiskGroup(0, 8*10**26, 6*10**26, ONE, ONE);
-        assertEq(navFeed.values_uint("file_thresholdRatio_"), 8*10**26);
+        (uint128 ceilingRatio,,) = navFeed.riskGroup(0);
+        emit log_named_uint("test", ceilingRatio);
+        // assertEq(ceilingRatio, 6*10**26);
     }
 
     function testAddRiskGroup() public {
@@ -79,9 +83,64 @@ contract RiskManagementPoolAdminTest is DSTest {
 
     // TODO: testAddRiskGroups
 
+    function callAddRiskGroups(uint256[] memory risks_, uint256[] memory thresholdRatios_, uint256[] memory ceilingRatios_, uint256[] memory rates_, uint256[] memory recoveryRatePDs_) public {
+        poolAdmin.addRiskGroups(risks_, thresholdRatios_, ceilingRatios_, rates_, recoveryRatePDs_);
+        // emit log_named_uint("test", navFeed.calls("addRiskGroup"));
+        // assertEq(navFeed.calls("addRiskGroup"), 3);
+    }
+
+    // function callFailAddRiskGroupsDifferentArrayLength(uint256[] risks_, uint256[] thresholdRatios_, uint256[] ceilingRatios_, uint256[] rates_, uint256[] recoveryRatePDs_) public {
+    //     poolAdmin.addRiskGroups(risks_, thresholdRatios_, ceilingRatios_, rates_, recoveryRatePDs_);
+    //     assertEq(navFeed.calls("addRiskGroup"), 3);
+    // }
+
+    function testAddRiskGroups() public {
+        navFeed.rely(address(this));
+        uint[] memory risks_ = new uint[](3);
+        uint[] memory thresholdRatios_ = new uint[](3);
+        uint[] memory ceilingRatios_ = new uint[](3);
+        uint[] memory rates_ = new uint[](3);
+        uint[] memory recoveryRatePDs_ = new uint[](3);
+        risks_[0] = 0;
+        risks_[1] = 1;
+        risks_[2] = 2;
+        thresholdRatios_[0] = 8*10**26;
+        thresholdRatios_[1] = 8*10**26;
+        thresholdRatios_[2] = 8*10**26;
+        ceilingRatios_[0] = 6*10**26;
+        ceilingRatios_[1] = 6*10**26;
+        ceilingRatios_[2] = 6*10**26;
+        rates_[0] = ONE;
+        rates_[1] = ONE;
+        rates_[2] = ONE;
+        recoveryRatePDs_[0] = ONE;
+        recoveryRatePDs_[0] = ONE;
+        recoveryRatePDs_[0] = ONE;
+        callAddRiskGroups(risks_, thresholdRatios_, ceilingRatios_, rates_, recoveryRatePDs_);
+    }
+
+    function testFailAddRiskGroups() public {
+        uint[] memory risks_ = new uint[](3);
+        uint[] memory thresholdRatios_ = new uint[](3);
+        uint[] memory ceilingRatios_ = new uint[](3);
+        uint[] memory rates_ = new uint[](2);
+        uint[] memory recoveryRatePDs_ = new uint[](3);
+        risks_[0] = 0;
+        risks_[1] = 1;
+        thresholdRatios_[0] = 8*10**26;
+        thresholdRatios_[1] = 8*10**26;
+        ceilingRatios_[0] = 6*10**26;
+        ceilingRatios_[1] = 6*10**26;
+        rates_[0] = ONE;
+        rates_[1] = ONE;
+        recoveryRatePDs_[0] = ONE;
+        recoveryRatePDs_[0] = ONE;
+        callAddRiskGroups(risks_, thresholdRatios_, ceilingRatios_, rates_, recoveryRatePDs_);
+    }
+
     function callAddWriteOffGroup() public {
         poolAdmin.addWriteOffGroup(uint(1000000674400000000000000000), 75 * 10**25, 30);
-        assertEq(navFeed.values_uint("file_writeOffPercentage"), 75 * 10**25);
+        assertEq(navFeedMock.values_uint("file_writeOffPercentage"), 75 * 10**25);
     }
 
     function testAddWriteOffGroup() public {
@@ -108,8 +167,8 @@ contract RiskManagementPoolAdminTest is DSTest {
         bytes32 nftID_ = "1";
         uint value = 100;
         poolAdmin.updateNFTValue(nftID_, value);
-        assertEq(navFeed.values_bytes32("update_nftID"), nftID_);
-        assertEq(navFeed.values_uint("update_value"), value);
+        assertEq(navFeedMock.values_bytes32("update_nftID"), nftID_);
+        assertEq(navFeedMock.values_uint("update_value"), value);
     }
 
     function testUpdateNFTValue() public {
@@ -126,9 +185,9 @@ contract RiskManagementPoolAdminTest is DSTest {
         uint value = 100;
         uint risk = 0;
         poolAdmin.updateNFTValueRisk(nftID_, value, risk);
-        assertEq(navFeed.values_bytes32("update_nftID"), nftID_);
-        assertEq(navFeed.values_uint("update_value"), value);
-        assertEq(navFeed.values_uint("update_risk"), risk);
+        assertEq(navFeedMock.values_bytes32("update_nftID"), nftID_);
+        assertEq(navFeedMock.values_uint("update_value"), value);
+        assertEq(navFeedMock.values_uint("update_risk"), risk);
     }
 
     function testUpdateNFTValueRisk() public {
@@ -144,8 +203,8 @@ contract RiskManagementPoolAdminTest is DSTest {
         bytes32 nftID_ = "1";
         uint maturityDate = block.timestamp + 4 weeks;
         poolAdmin.updateNFTMaturityDate(nftID_, maturityDate);
-        assertEq(navFeed.values_bytes32("file_nftID"), nftID_);
-        assertEq(navFeed.values_uint("file_maturityDate"), maturityDate);
+        assertEq(navFeedMock.values_bytes32("file_nftID"), nftID_);
+        assertEq(navFeedMock.values_uint("file_maturityDate"), maturityDate);
     }
 
     function testUpdateNFTMaturityDate() public {
