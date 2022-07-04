d698e708aedb1c4b1086d90095aca77cf1ecd441
Adam Stockermans <adamstox@protonmail.com>
2022-07-04 16:16:22 -0400

    fix addresses in rates mapping

--- src/borrower/wrappers/writeoffWrapper.sol
+++ src/borrower/wrappers/writeoffWrapper.sol
@@ -22,23 +22,23 @@ contract WriteoffWrapper is Auth, Discounting {
         wards[msg.sender] = 1;
         emit Rely(msg.sender);
 
-        writeoffRates[address(0x09e43329552c9D81cF205Fd5f44796fBC40c822e)] = 0;  // REIF Pool ?? https://centrifuge.hackmd.io/31xg2b6jTLWzJdFvOngq0g?view
-        writeoffRates[address(0x0CED6166873038Ac0cc688e7E6d19E2cBE251Bf0)] = 1000;  // Bling Series 1
-        writeoffRates[address(0x235893Bf9695F68a922daC055598401D832b538b)] = 1001;  // Pezesha 1
-        writeoffRates[address(0x3170D353772eD68676044f8b76F0641B2cbA084E)] = 0;  // Fortunafi 2  ?? https://centrifuge.hackmd.io/4vTa41y6QFmA3x-OogkenQ
-        writeoffRates[address(0x3B03863BD553C4CE07eABF2278016533451c9101)] = 0;  // Cauris Global Fintech 1 ?? https://centrifuge.hackmd.io/2Vxnf3VmRU6lveB3xnrmFQ
-        writeoffRates[address(0x3d167bd08f762FD391694c67B5e6aF0868c45538)] = 1001;  // GIG Pool
-        writeoffRates[address(0x4B6CA198d257D755A5275648D471FE09931b764A)] = 1000;  // Fortunafi 1
-        writeoffRates[address(0x4cA805cE8EcE2E63FfC1F9f8F2731D3F48DF89Df)] = 1000;  // Harbor Trade 2
-        writeoffRates[address(0x53b2d22d07E069a3b132BfeaaD275b10273d381E)] = 1000;  // New Silver 2
-        writeoffRates[address(0x560Ac248ce28972083B718778EEb0dbC2DE55740)] = 1001;  // Branch Series 3
-        writeoffRates[address(0x714D520CfAC2027834c8Af8ffc901855c3aD41Ec)] = 1000;  // FactorChain 1
-        writeoffRates[address(0x82B8617A16e388256617FeBBa1826093401a3fE5)] = 1000;  // Paperchain 3
-        writeoffRates[address(0x92332a9831AC04275bC0f22b9140b21c72984EB8)] = 1001;  // Pezesha 1
-        writeoffRates[address(0x9De3064f49696a25066252C35ede68850EA33BF8)] = 1001;  // UP Series 1
-        writeoffRates[address(0xdB3bC9fB1893222d266762e9fF857EB74D75c7D6)] = 1001;  // ConsolFreight 4
-        writeoffRates[address(0xF96F18F2c70b57Ec864cC0C8b828450b82Ff63e3)] = 0;  // ALT 1.0  ?? https://centrifuge.hackmd.io/FBFX0lldR9iFSZexHtCLuQ
-        writeoffRates[address(0xfc2950dD337ca8496C18dfc0256Fb905A7E7E5c6)] = 1000;  // databased.FINANCE 1
+        writeoffRates[address(0xc2a75288cf644b068fC1e765586c170f2529a87F)] = 0;  // REIF Pool ?? https://centrifuge.hackmd.io/31xg2b6jTLWzJdFvOngq0g?view
+        writeoffRates[address(0x05739C677286d38CcBF0FfC8f9cdbD45904B47Fd)] = 1000;  // Bling Series 1
+        writeoffRates[address(0xAAEaCfcCc3d3249f125Ba0644495560309C266cB)] = 1001;  // Pezesha 1
+        writeoffRates[address(0x2bD53Bd2341329e0cf36c55380A77a3E457028Ca)] = 0;  // Fortunafi 2  ?? https://centrifuge.hackmd.io/4vTa41y6QFmA3x-OogkenQ
+        writeoffRates[address(0x696866ea53C0f521C24a28D92fAbD2c559182D38)] = 0;  // Cauris Global Fintech 1 ?? https://centrifuge.hackmd.io/2Vxnf3VmRU6lveB3xnrmFQ
+        writeoffRates[address(0x9E39e0130558cd9A01C1e3c7b2c3803baCb59616)] = 1001;  // GIG Pool
+        writeoffRates[address(0x11C14AAa42e361Cf3500C9C46f34171856e3f657)] = 1000;  // Fortunafi 1
+        writeoffRates[address(0xE7876f282bdF0f62e5fdb2C63b8b89c10538dF32)] = 1000;  // Harbor Trade 2
+        writeoffRates[address(0x3eC5c16E7f2C6A80E31997C68D8Fa6ACe089807f)] = 1000;  // New Silver 2
+        writeoffRates[address(0xe17F3c35C18b2Af84ceE2eDed673c6A08A671695)] = 1001;  // Branch Series 3
+        writeoffRates[address(0x0168e7999318a6c2393c2Eb19A5Da4aB9d715173)] = 1000;  // FactorChain 1
+        writeoffRates[address(0x37c8B836eA1b89b7cC4cFdDed4C4fbC454CcC679)] = 1000;  // Paperchain 3
+        writeoffRates[address(0xAAEaCfcCc3d3249f125Ba0644495560309C266cB)] = 1001;  // Pezesha 1
+        writeoffRates[address(0xB7d1DE24c0243e6A3eC4De9fAB2B19AB46Fa941F)] = 1001;  // UP Series 1
+        writeoffRates[address(0x3fC72dA5545E2AB6202D81fbEb1C8273Be95068C)] = 1001;  // ConsolFreight 4
+        writeoffRates[address(0xE18AAB16cC26EB23740D72875e0C6b52cEbb46b3)] = 0;  // ALT 1.0  ?? https://centrifuge.hackmd.io/FBFX0lldR9iFSZexHtCLuQ
+        writeoffRates[address(0xdB07B21109117208a0317adfbed484C87c9c2aFf)] = 1000;  // databased.FINANCE 1
     }
     
     function writeOff(uint _loanID, address _pile, address _feed) public auth {
@@ -50,7 +50,7 @@ contract WriteoffWrapper is Auth, Discounting {
         
         require(maturityDate <= nnow, "loan isn't overdue yet.");
 
-        pile.changeRate(_loanID, writeoffRates[address(pile)]);
+        pile.changeRate(_loanID, writeoffRates[_pile]);
     }
 
 }
