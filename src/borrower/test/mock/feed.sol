7844b8998ed6484331c5969da622147ec51e3047
Adam Stox <AStox@users.noreply.github.com>
2022-11-07 10:07:54 -0500

    Writeoff wrapper (#613)
    * Add writeoff wrapper contract
    
    * Fixing build errors
    
    * WIP: add tests
    
    * Fixed tests. add rates mapping
    
    * fix addresses in rates mapping
    
    * Remove comment
    
    * Switch WriteOff() to only use NAV address. Update tests accordingly
    
    * rename instances of writeoff to writeOff
    
    * Add more tests
    
    * update file names
    
    * update hardcoded pile info
    
    * remove TODO

---
+++ src/borrower/test/mock/feed.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.7.6;
+import "ds-test/test.sol";
+
+import "../../../test/mock/mock.sol";
+import "tinlake-auth/auth.sol";
+
+contract NAVFeedMock is Mock, Auth {
+    constructor() {
+        wards[msg.sender] = 1;
+    }
+
+    function pile() public view returns(address){ 
+            return values_address_return["pile"];
+    }
+
+    function shelf() public view returns(address){ 
+            return values_address_return["shelf"];
+    }
+
+    function maturityDate(bytes32 nft_) public view returns(uint){ 
+            return values_return["maturityDate"];
+    }
+
+    function nftID(uint loan) public returns (bytes32) {
+        return values_bytes32_return["nftID"];
+    }
+}
