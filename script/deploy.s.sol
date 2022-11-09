4663135bc02961bea37903b6dcfed30f42a055ee
Jeroen Offerijns <jeroen@offerijns.com>
2022-11-09 13:49:21 +0100

    Fix factory deployments

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -111,7 +111,23 @@ contract TinlakeDeployScript is Script {
 
     function getOrDeployFab(string memory contractPath) internal returns (address) {
         bytes memory initCode = vm.getCode(contractPath);
-        address deploymentAddress = factory.findCreate2Address(salt, initCode);
+
+        // Taken from https://github.com/0age/metamorphic/blob/master/contracts/ImmutableCreate2Factory.sol#L153
+        address deploymentAddress = address(
+            uint160( // downcast to match the address type.
+                uint256( // convert to uint to truncate upper digits.
+                    keccak256( // compute the CREATE2 hash using 4 inputs.
+                        abi.encodePacked( // pack all inputs to the hash together.
+                            hex"ff", // start with 0xff to distinguish from RLP.
+                            address(factory), // this contract will be the caller.
+                            salt, // pass in the supplied salt value.
+                            keccak256( // pass in the hash of initialization code.
+                            abi.encodePacked(initCode))
+                        )
+                    )
+                )
+            )
+        );
 
         if (factory.hasBeenDeployed(deploymentAddress)) {
             return deploymentAddress;
