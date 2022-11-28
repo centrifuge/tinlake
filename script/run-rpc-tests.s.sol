840bdfb1c8e065298e34142a015c0dd20f5bb2b4
Manuel Polzhofer <manuel.polzhofer@protonmail.com>
2022-11-28 12:12:45 +0100

    rpc-tests: deactivate clerk for basic tests (#629)
    * rpc-tests: deactivate clerk for basis tests
    
    * test: clean up

--- script/run-rpc-tests.s.sol
+++ script/run-rpc-tests.s.sol
@@ -9,13 +9,39 @@ import {TinlakeRPCTests} from "./rpc-tests/rpc-tests.sol";
 contract RunRPCTests is Script, TinlakeRPCTests {
     function setUp() public {}
 
+    function _isMakerLive() internal returns (bool) {
+        return mgr.urn() != address(0) && clerk.activated();
+    }
+
+    function _deactivateClerk() internal {
+        if (address(assessor.lending()) != address(0)) {
+            console.log("PreRPC Test: remove clerk dependency from assessor");
+            root.relyContract(address(assessor), address(this));
+            assessor.depend("lending", address(0));
+        }
+
+        if (address(reserve.lending()) != address(0)) {
+            console.log("PreRPC Test: remove clerk dependency from reserve");
+            root.relyContract(address(reserve), address(this));
+            reserve.depend("lending", address(0));
+        }
+    }
+
     function run() public {
         initRPC(vm.envAddress("ROOT_CONTRACT"));
-
         if (vm.envBool("MAKER_RPC_TESTS") == true) {
-            runLoanCycleWithMaker();
+            console.log("Running: Maker RPC tests");
+            if (_isMakerLive()) {
+                runLoanCycleWithMaker();
+            } else {
+                revert("Maker is not live");
+            }
         } else {
+            // deactivate clerk to run without maker
+            console.log("Running: Basic RPC tests");
+            _deactivateClerk();
             runLoanCycleWithoutMaker();
         }
+        console.log("RPC tests passed");
     }
 }
